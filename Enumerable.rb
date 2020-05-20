9945f2f195ed4f8770b1491db5c60b34e17d3cf5
afcastaneda223 <af.castaneda223@gmail.com>
2020-05-20 00:46:13 -0300

    add my_inject method;

--- Enumerable.rb
+++ Enumerable.rb
@@ -75,58 +75,87 @@ module Enumerable
         }
         return pick
     end
-    def my_map
+    def my_map(param = nil)
         maps = []
         self.my_each { |x|
-            maps.push(yield(x))
+            if param
+                maps.push(param.call(x))
+                elsif block_given?
+                maps.push(yield(x))
+                else
+                return to_enum
+            end
         }
         return maps
     end
     #my_inject
+    def my_inject
+        store = self[0]
+        self.my_map{|x| 
+        store = yield(store, x) 
+    }
+    end
+    def multiply_els(arr)
+        arr.my_inject{|x,y| 
+        x*y
+    }
+    end
 end
 
 array = ["a","b","c",0,1,2,3,true,false]
 num_array = [1,2,3,4,5,6,7]
 string_array = ["ab","abc","abcd"]
 bool_array = [true,false,true]
+my_proc = Proc.new { |x| x+2}
+
+puts " my_each"
 
 array.my_each { |x|
-    puts x
-}
-puts "my_each"
-
-array.my_each_with_index { |x,y|
-    puts x if y.odd?
-}
-puts "my_each_with_index"
-
-puts num_array.my_select {|x| x.odd?}
-
-puts string_array.my_select {|x| x != "a"}
-
-puts "my_select"
-
-puts string_array.my_all? {|x| x.length >3}
-
-puts "my_all"
-
-puts string_array.my_any? {|x| x.length >3}
-
-puts "my_any"
-
-puts num_array.count
-
-puts num_array.count{|x|
-    x >=4
-}
-puts "my_count"
-
-print num_array.my_map{|x| 
-    x+5
+    print x
 }
 puts " "
+puts " my_each_with_index"
+
+print array.my_each_with_index { |x,y|
+     x if y.odd?
+}
+puts " "
+puts " my_select"
+
+print num_array.my_select {|x| x.odd?}
+
+print string_array.my_select {|x| x != "a"}
+
+puts " "
+puts " my_all"
+
+print string_array.my_all? {|x| x.length >3}
+
+puts " "
+puts " my_any"
+
+
+print string_array.my_any? {|x| x.length >3}
+
+puts " "
+puts "my_count"
+
+puts num_array.count
+puts num_array.count{|x|
+    x >= 4
+}
+
 puts "my_map"
 
+print num_array.my_map{|x| 
+    x+1
+}
+
+print num_array.my_map(&my_proc)
 
 
+puts " "
+puts "my_inject"
 
+
+print multiply_els(num_array)
--- test.rb
+++ test.rb
@@ -1,6 +1,43 @@
-print array = ["a","b","c",1,2,3,true,false]
-num_array = [1,2,3,4,5,6,7]
+module Enumerable
+    def my_each
+        n= self.length
+        x = 0
+        while x < n
+            yield(self[x])
+            x += 1
+        end
+        return self
+    end
+
+    def my_map(param = nil)
+        maps = []
+        self.my_each { |x|
+            if param
+                maps.push(param.call(x))
+                elsif block_given?
+                maps.push(yield(x))
+                else
+                return to_enum
+            end
+        }
+        return maps
+    end
+    def my_inject
+        store = self[0]
+        self.my_map{|x| 
+        store = yield(store[x], x) 
+    }
+    end
+end
+array = ["a","b","c",1,2,3,true,false]
+num_array = [1,2,3]
 string_array = ["ab","bbbb","cb"]
 bool_array = [true,false,true]
+my_proc = Proc.new { |x,y| x}
 
-print num_array.map{|x| x+1}
+
+
+#print num_array.map{|x| x+1}
+
+
+print num_array.my_inject(&my_proc)
