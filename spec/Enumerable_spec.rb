3affa43cdf3d7e6df85564ed620167988c72581a
mke2111 <roymke134@gmail.com>
2020-10-27 22:48:31 +0300

    added test for my_any?

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -48,38 +48,50 @@ describe Enumerable do
   #   end
   # end
 
-    describe '#my_all?' do
-    let(:array1) { [] }
-    let(:array2) { [nil, true, 99] }
-    let(:array3) { [1, 2i, 3.14]   }
-    let(:array4) { %w[ant bear cat] }
-    it 'When no block is given and empty array' do
-        expect(array1.my_all?).to be true  
-    end
-    it 'When no block is given and not empty but contains nil' do
-        expect(array2.my_all?).to be false  
-    end
-    it 'When no block is given and imput is Numeric' do
-        expect(array3.my_all?(Numeric)).to be true  
-    end
-    it 'When no block is given and imput is Regex' do
-        expect(array4.my_all?(/t/)).to be false  
-    end
-    it 'When block is given' do
-        expect(array4.my_all?{ |word| word.length >= 3 }).to be true  
-    end
+    # describe '#my_all?' do
+    #   let(:array1) { [] }
+    #   let(:array2) { [nil, true, 99] }
+    #   let(:array3) { [1, 2i, 3.14]   }
+    #   let(:array4) { %w[ant bear cat] }
+    #   it 'When no block is given and empty array' do
+    #       expect(array1.my_all?).to be true  
+    #   end
+    #   it 'When no block is given and not empty but contains nil' do
+    #       expect(array2.my_all?).to be false  
+    #   end
+    #   it 'When no block is given and imput is Numeric' do
+    #       expect(array3.my_all?(Numeric)).to be true  
+    #   end
+    #   it 'When no block is given and imput is Regex' do
+    #       expect(array4.my_all?(/t/)).to be false  
+    #   end
+    #   it 'When block is given' do
+    #       expect(array4.my_all?{ |word| word.length >= 3 }).to be true  
+    #   end
+    # end
 
+    describe '#my_any?' do
+      let(:array1) { [] }
+      let(:array2) { [nil, true, 99] }
+      let(:array3) { [1, 2i, 3.14]   }
+      let(:array4) { %w[ant bear cat] }
 
-    
-end
-#     it 'When a block is given' do
-#       expect(array.my_select { |x| x }).to be_a(Array || Hash)
-#     end
-
-#     it 'with argument given, does it yield?' do
-#       expect(array.my_select { |x| x > 2 }).to eq ([4, 6])
-#     end
-#   end
+      it 'When no block is given and empty array' do
+          expect(array1.my_any?).to be false  
+      end
+      it 'When no block is given and not empty but contains nil' do
+          expect(array2.my_any?).to be true    
+      end
+      it 'When no block is given and imput is Numeric' do
+          expect(array3.my_any?(Numeric)).to be true  
+      end
+      it 'When no block is given and imput is Regex' do
+          expect(array4.my_any?(/d/)).to be false  
+      end
+      it 'When block is given' do
+          expect(array4.my_any?{ |word| word.length >= 4 }).to be true  
+      end
+    end
 
 end
 
