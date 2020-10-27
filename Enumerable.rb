f65d2691202c7c2e3ef1d2cac8f28d1cf1005918
afcastaneda223 <af.castaneda223@gmail.com>
2020-10-27 14:20:26 -0300

    added test for my_each_with_index

--- Enumerable.rb
+++ Enumerable.rb
@@ -211,3 +211,8 @@ end
 def multiply_els(arr)
   arr.my_inject { |x, y| x * y }
 end
+
+
+arr = [1,2,3]
+
+arr.my_each_with_index{|x,y|puts "index: #{x} for #{y}"}
--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -17,5 +17,21 @@ describe Enumerable do
 
   end
 
+  describe '#my_each_with_index' do
+    let(:array) { [1, 4, 6] }
+    it 'When no block is given' do
+      expect(array.my_each_with_index).to be_a(Enumerable)
+    end
+
+    it 'When a block is given' do
+      expect(array.my_each_with_index { |x,y| [x,y] }).to be_a(Array || Hash)
+    end
+
+    it 'with argument given, does it yield?' do
+      expect { |x| array.my_each_with_index(&x)}.to yield_successive_args([1, 0], [4, 1], [6, 2])
+    end
+
+  end
 
 end
+
