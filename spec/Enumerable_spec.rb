4a5344fce5b8eb8634cfa936825720eee2ebd4ee
mke2111 <roymke134@gmail.com>
2020-10-27 20:29:28 +0300

    Added test for My_select

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -1,37 +1,52 @@
 require './Enumerable.rb'
 
 describe Enumerable do
-  describe '#my_each' do
-    let(:array) { [1, 4, 6] }
-    it 'When no block is given' do
-      expect(array.my_each).to be_a(Enumerable)
-    end
+  # describe '#my_each' do
+  #   let(:array) { [1, 4, 6] }
+  #   it 'When no block is given' do
+  #     expect(array.my_each).to be_a(Enumerable)
+  #   end
 
-    it 'When a block is given' do
-      expect(array.my_each { |x| x }).to be_a(Array || Hash)
-    end
+  #   it 'When a block is given' do
+  #     expect(array.my_each { |x| x }).to be_a(Array || Hash)
+  #   end
 
-    it 'with argument given, does it yield?' do
-      expect { |x| array.my_each(&x) }.to yield_successive_args(1, 4, 6)
-    end
+  #   it 'with argument given, does it yield?' do
+  #     expect { |x| array.my_each(&x) }.to yield_successive_args(1, 4, 6)
+  #   end
 
-  end
+  # end
 
-  describe '#my_each_with_index' do
-    let(:array) { [1, 4, 6] }
-    it 'When no block is given' do
-      expect(array.my_each_with_index).to be_a(Enumerable)
-    end
+  # describe '#my_each_with_index' do
+  #   let(:array) { [1, 4, 6] }
+  #   it 'When no block is given' do
+  #     expect(array.my_each_with_index).to be_a(Enumerable)
+  #   end
 
-    it 'When a block is given' do
-      expect(array.my_each_with_index { |x,y| [x,y] }).to be_a(Array || Hash)
-    end
+  #   it 'When a block is given' do
+  #     expect(array.my_each_with_index { |x,y| [x,y] }).to be_a(Array || Hash)
+  #   end
 
-    it 'with argument given, does it yield?' do
-      expect { |x| array.my_each_with_index(&x)}.to yield_successive_args([1, 0], [4, 1], [6, 2])
-    end
+  #   it 'with argument given, does it yield?' do
+  #     expect { |x| array.my_each_with_index(&x)}.to yield_successive_args([1, 0], [4, 1], [6, 2])
+  #   end
 
-  end
+  # end
+
+  # describe '#my_select' do
+  #   let(:array) { [1, 4, 6] }
+  #   it 'When no block is given' do
+  #     expect(array.my_select).to be_a(Enumerable)
+  #   end
+
+  #   it 'When a block is given' do
+  #     expect(array.my_select { |x| x }).to be_a(Array || Hash)
+  #   end
+
+  #   it 'with argument given, does it yield?' do
+  #     expect(array.my_select { |x| x > 2 }).to eq ([4, 6])
+  #   end
+  # end
 
 end
 
