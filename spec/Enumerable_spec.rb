9dd1568484f3580baa913c6c4ba8a27ab3d38de9
mke2111 <roymke134@gmail.com>
2020-10-27 19:47:34 +0300

    added tests for my_each

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -1,2 +1,21 @@
-require relative 'Enumerable'
+require './Enumerable.rb'
 
+describe Enumerable do
+  describe '#my_each' do
+    let(:array) { [1, 4, 6] }
+    it 'When no block is given' do
+      expect(array.my_each).to be_a(Enumerable)
+    end
+
+    it 'When a block is given' do
+      expect(array.my_each { |x| x }).to be_a(Array || Hash)
+    end
+
+    it 'with argument given, does it yield?' do
+      expect { |x| array.my_each(&x) }.to yield_successive_args(1, 4, 6)
+    end
+
+  end
+
+
+end
